2f424e4ccccad8caa319344f21c7b46e5e1a51f2
Preston Sparks <mprestonsparks@gmail.com>
2019-02-25 12:11:40 -0600

    added function to select random question category

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,59 @@
 
 // TO DO
 // Randomize the order of the movies in the movies array & change array references in pickRandomMovies() to randomized list
-
+// --------------------------------------------------------------
 
 // DECLARE VARIABLES
+var movies = ["Cars", "The Notebook", "Mr. Nobody", "The Godfather"]; // Create array of movies
+var questionCategories = ["Actors","Director","Plot","Released","Runtime"]; // Categories of questions
+var questionCategory; // The type of question to be asked (plot, released date, etc.)
 var correctMovie;
 var incorrectMovieOne;
 var incorrectMovieTwo;
 // --------------------------------------------------------------
 
 // WRITE FUNCTIONS
+// Function to pick a random # (used in the pickRandomMovies function)
 function createRandom(min, max) {
     return Math.round(Math.floor(Math.random() * (max - min + 1)) + min);
 }
-  
-// Create array of movies
-var movies = ["Cars","The Notebook","Mr. Nobody","The Godfather"];
 
+// Pick a random movie from the movies array
 function pickRandomMovies() {
     var moviesArrayMin = 0; // First movie in movies array
-    var moviesArrayMax = movies.length-1; // # of items in movies array
-    correctMovie = movies[createRandom(moviesArrayMin,moviesArrayMax)];
+    var moviesArrayMax = movies.length - 1; // # of items in movies array
+    correctMovie = movies[createRandom(moviesArrayMin, moviesArrayMax)];
     var correctMovieIndex = movies.indexOf(correctMovie);
-    var correctMovieIsLast = (correctMovieIndex === movies.length-1);
+    var correctMovieIsLast = (correctMovieIndex === movies.length - 1);
     var correctMovieIsFirst = (correctMovieIndex === 0);
     if (correctMovieIsLast) {
-        incorrectMovieOne = movies[correctMovieIndex-2];
-        incorrectMovieTwo = movies[correctMovieIndex-3];
+        incorrectMovieOne = movies[correctMovieIndex - 2];
+        incorrectMovieTwo = movies[correctMovieIndex - 3];
+    } else {
+        incorrectMovieOne = movies[correctMovieIndex + 1];
+        if (correctMovieIsFirst) {
+            incorrectMovieTwo = movies[correctMovieIndex + 2];
         } else {
-        incorrectMovieOne = movies[correctMovieIndex+1];
-            if (correctMovieIsFirst) {
-                incorrectMovieTwo = movies[correctMovieIndex+2];
-            } else {
-            incorrectMovieTwo = movies[correctMovieIndex-1];
-            }
+            incorrectMovieTwo = movies[correctMovieIndex - 1];
         }
     }
- 
-    
+}
+
+// Pick a random category of question
+function pickRandomQuestion() {
+    var questionsArrayMin = 0; // First category in questionCategories array
+    var questionsArrayMax = questionCategories.length -1; // # of items in questionCategories array
+    questionCategory = questionCategories[createRandom(0,4)];
+}
 
 // Function to pull movie info from OMDB API
 function getMovieInfo() {
-
-    // var correctMovie = movies[0];
     var queryURL = "https://www.omdbapi.com/?t=" + correctMovie + "&y=&plot=short&apikey=trilogy";
-    
     // Create an AJAX call for the movie selected
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         var title = response.Title;
         var released = response.Released;
         var rating = response.Rating;
@@ -62,9 +66,11 @@ function getMovieInfo() {
 
 // CALL FUNCTIONS
 pickRandomMovies();
+pickRandomQuestion();
 getMovieInfo();
 // --------------------------------------------------------------
+
 console.log("correctMovie: " + correctMovie);
 console.log("incorrectMovieOne: " + incorrectMovieOne);
 console.log("incorrectMovieTwo: " + incorrectMovieTwo);
-console.log(movies.length);
+console.log("questionCategory: " + questionCategory);
