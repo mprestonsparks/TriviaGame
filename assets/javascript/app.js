ae3b9c18d078f6e89c9cb56775c324b1ba9c2c42
Preston Sparks <mprestonsparks@gmail.com>
2019-02-25 11:51:30 -0600

    finished pickRandomMovies function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,49 @@
 
-// Create array of movies
-var movies = ["Cars","The Notebook","Mr. Nobody"];
+// TO DO
+// Randomize the order of the movies in the movies array & change array references in pickRandomMovies() to randomized list
 
+
+// DECLARE VARIABLES
+var correctMovie;
+var incorrectMovieOne;
+var incorrectMovieTwo;
+// --------------------------------------------------------------
+
+// WRITE FUNCTIONS
+function createRandom(min, max) {
+    return Math.round(Math.floor(Math.random() * (max - min + 1)) + min);
+}
+  
+// Create array of movies
+var movies = ["Cars","The Notebook","Mr. Nobody","The Godfather"];
+
+function pickRandomMovies() {
+    var moviesArrayMin = 0; // First movie in movies array
+    var moviesArrayMax = movies.length-1; // # of items in movies array
+    correctMovie = movies[createRandom(moviesArrayMin,moviesArrayMax)];
+    var correctMovieIndex = movies.indexOf(correctMovie);
+    var correctMovieIsLast = (correctMovieIndex === movies.length-1);
+    var correctMovieIsFirst = (correctMovieIndex === 0);
+    if (correctMovieIsLast) {
+        incorrectMovieOne = movies[correctMovieIndex-2];
+        incorrectMovieTwo = movies[correctMovieIndex-3];
+        } else {
+        incorrectMovieOne = movies[correctMovieIndex+1];
+            if (correctMovieIsFirst) {
+                incorrectMovieTwo = movies[correctMovieIndex+2];
+            } else {
+            incorrectMovieTwo = movies[correctMovieIndex-1];
+            }
+        }
+    }
+ 
+    
+
+// Function to pull movie info from OMDB API
 function getMovieInfo() {
 
-    // var movie = $(this).attr("data-name");
-    var movie = movies[0];
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    // var correctMovie = movies[0];
+    var queryURL = "https://www.omdbapi.com/?t=" + correctMovie + "&y=&plot=short&apikey=trilogy";
     
     // Create an AJAX call for the movie selected
     $.ajax({
@@ -18,8 +55,16 @@ function getMovieInfo() {
         var rating = response.Rating;
         var plot = response.Plot;
         var imgURL = response.Poster;
-        console.log("Title..",title);
+        console.log(response);
     });
 }
+// --------------------------------------------------------------
 
+// CALL FUNCTIONS
+pickRandomMovies();
 getMovieInfo();
+// --------------------------------------------------------------
+console.log("correctMovie: " + correctMovie);
+console.log("incorrectMovieOne: " + incorrectMovieOne);
+console.log("incorrectMovieTwo: " + incorrectMovieTwo);
+console.log(movies.length);
