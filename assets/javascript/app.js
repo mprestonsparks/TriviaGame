b2cd0f8e52b318432e9d88439b299b570cbd1670
Preston <mprestonsparks@gmail.com>
2019-02-27 23:36:51 -0600

    lot of crap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,27 @@
 // TO DO
 // Randomize the order of the movies in the movies array & change array references in pickRandomMovies() to randomized list
 // Fix the answer column size (bootsrap isn't working), or get rid of "padding" columns and add padding to answer column
-// Fix the buttons on the modal-- see https://www.youtube.com/watch?v=MLtAMg9_Svw
+
+// ISSUES:
+// On refresh, the first 2 guesses are correct (ie the console.log of correctAnswerDiv# matches the HTML on screen)
+// But the 3rd+ one breaks every time
+
+//There's a circular feedback loop bw checkAnswer() and changeQuestion(), so probably checkAnswer() never gets
+//called again after refresh (?)
 // --------------------------------------------------------------
 
-// DECLARE VARIABLES
-var movies = ["The Shawshank Redemption","The Godfather","The Dark Knight","The Godfather: Part II","The Lord of the Rings: The Return of the King","Pulp Fiction","Schindler's List","The Good, the Bad and the Ugly","12 Angry Men","Inception","Fight Club","The Lord of the Rings: The Fellowship of the Ring","Forrest Gump","Star Wars: Episode V","The Lord of the Rings: The Two Towers","The Matrix","Goodfellas","Seven Samurai","Andhadhun","Interstellar","City of God","Spirited Away","Saving Private Ryan","Life Is Beautiful","The Usual Suspects","Se7en","Léon: The Professional","The Silence of the Lambs","Star Wars","It's a Wonderful Life","Dangal","Whiplash","The Prestige","The Departed","The Pianist","Memento","Gladiator","The Green Mile","American History X","The Lion King","Back to the Future","Raiders of the Lost Ark","Apocalypse Now"];
+// ********** VARIABLES **********
+var movies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "The Godfather: Part II", "The Lord of the Rings: The Return of the King", "Pulp Fiction", "Schindler's List", "The Good, the Bad and the Ugly", "12 Angry Men", "Inception", "Fight Club", "The Lord of the Rings: The Fellowship of the Ring", "Forrest Gump", "Star Wars: Episode V", "The Lord of the Rings: The Two Towers", "The Matrix", "Goodfellas", "Seven Samurai", "Andhadhun", "Interstellar", "City of God", "Spirited Away", "Saving Private Ryan", "Life Is Beautiful", "The Usual Suspects", "Se7en", "Léon: The Professional", "The Silence of the Lambs", "Star Wars", "It's a Wonderful Life", "Dangal", "Whiplash", "The Prestige", "The Departed", "The Pianist", "Memento", "Gladiator", "The Green Mile", "American History X", "The Lion King", "Back to the Future", "Raiders of the Lost Ark", "Apocalypse Now"];
 var correctMovie;
 var incorrectMovieOne;
 var incorrectMovieTwo;
-var questionCategories = ["Actors","Director","Plot","Released","Runtime","Plot"]; // Categories of questions
+var questionCategories = ["Actors", "Director", "Plot", "Released", "Runtime", "Plot"]; // Categories of questions
 var questionCategory; // The type of question to be asked (plot, released date, etc.)
 var questionGrammarSubject;
-var answer;
+var answer; // 
 var correctAnswer;
+var incorrectAnswerOne;
+var incorrectAnswerTwo;
 var correctAnswerDiv; // The div holding the correct answer
 var incorrectAnswer1Div;
 var incorrectAnswer2Div;
@@ -22,12 +30,19 @@ var correctScore = 0;
 var incorrectScore = 0;
 // --------------------------------------------------------------
 
-// WRITE FUNCTIONS
+// ********** FUNCTIONS **********
 // Function to pick a random # (used in pickRandomMovies and pickRandomQuestion functions)
 function createRandom(min, max) {
     return Math.round(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
+function initializeNewGame() {
+    pickRandomMovies();
+    pickRandomQuestion();
+    newAJAXRequest();
+    randomizeAnswerDivs();
+    checkAnswer();
+}
 // Pick a random movie from the movies array
 function pickRandomMovies() {
     var moviesArrayMin = 0; // First movie in movies array
@@ -51,8 +66,8 @@ function pickRandomMovies() {
 // Pick a random category of question
 function pickRandomQuestion() {
     var questionsArrayMin = 0; // First category in questionCategories array
-    var questionsArrayMax = questionCategories.length -1; // # of items in questionCategories array
-    questionCategory = questionCategories[createRandom(0,4)];
+    var questionsArrayMax = questionCategories.length - 1; // # of items in questionCategories array
+    questionCategory = questionCategories[createRandom(0, 4)];
 
     switch (questionCategory) {
         case "Actors":
@@ -87,33 +102,31 @@ function correctMovieInfo() {
         var runtime = response.Runtime;
         var plot = response.Plot;
         var imgURL = response.Poster;
-        console.log(response);
         switch (String(questionCategory)) {
             case "Actors":
-                answer = actors;
+                correctAnswer = actors;
                 break;
             case "Director":
-                answer = director;
+                correctAnswer = director;
                 break;
             case "Plot":
-                answer = plot;
+                correctAnswer = plot;
                 break;
             case "Released":
-                answer = released;
+                correctAnswer = released;
                 break;
             case "Runtime":
-                answer = runtime;
+                correctAnswer = runtime;
                 break;
-            case "Plot": 
-                answer = plot;
+            case "Plot":
+                correctAnswer = plot;
         }
-        console.log("answer: " + answer);
         $("#movieImage").empty();
         var image = $("<img>").attr("src", imgURL); // Create element to hold the image
         $("#movieImage").append(image); // Replace the image
         $("#questionText").text(questionGrammarSubject + title + "?");
-        $(correctAnswerDiv).text(answer);
-        correctAnswer = answer;
+        $(correctAnswerDiv).text(correctAnswer);
+        // correctAnswer = answer;
     });
 }
 
@@ -131,25 +144,24 @@ function incorrectMovieOneInfo() {
         var plot = response.Plot;
         switch (String(questionCategory)) {
             case "Actors":
-                answer = actors;
+                incorrectAnswerOne = actors;
                 break;
             case "Director":
-                answer = director;
+                incorrectAnswerOne = director;
                 break;
             case "Plot":
-                answer = plot;
+                incorrectAnswerOne = plot;
                 break;
             case "Released":
-                answer = released;
+                incorrectAnswerOne = released;
                 break;
             case "Runtime":
-                answer = runtime;
+                incorrectAnswerOne = runtime;
                 break;
-            case "Plot": 
-                answer = plot;
+            case "Plot":
+                incorrectAnswerOne = plot;
         }
-        console.log("incorrectAnswerOne: " + answer);
-        $(incorrectAnswer1Div).text(answer);
+        $(incorrectAnswer1Div).text(incorrectAnswerOne);
     });
 }
 
@@ -167,25 +179,76 @@ function incorrectMovieTwoInfo() {
         var plot = response.Plot;
         switch (String(questionCategory)) {
             case "Actors":
-                answer = actors;
+                incorrectAnswerTwo = actors;
                 break;
             case "Director":
-                answer = director;
+                incorrectAnswerTwo = director;
                 break;
             case "Plot":
-                answer = plot;
+                incorrectAnswerTwo = plot;
                 break;
             case "Released":
-                answer = released;
+                incorrectAnswerTwo = released;
                 break;
             case "Runtime":
-                answer = runtime;
+                incorrectAnswerTwo = runtime;
                 break;
-            case "Plot": 
-                answer = plot;
+            case "Plot":
+                incorrectAnswerTwo = plot;
         }
-        console.log("incorrectAnswerTwo: " + answer);
-        $(incorrectAnswer2Div).text(answer);
+        $(incorrectAnswer2Div).text(incorrectAnswerTwo);
+    });
+}
+
+
+console.log("correct", correctAnswer);
+console.log("incorrect1", incorrectAnswerOne);
+var answerOrder=[];
+function randomizeAnswerDivs() {
+    // var answerOrder = [] // Create an array to set random positions of answers
+    answerOrder.length = 0;
+    var pickRandom = createRandom(0, 2);
+    answerOrder.push(pickRandom); // Pick one random number as the first position in answerOrder[]
+    for (var i = 0; i <= 2; i++) {
+        if (answerOrder.indexOf(i) == -1)
+            answerOrder.push(i);
+    }
+    correctAnswerDiv = "#answerDiv" + answerOrder[0];
+    correctAnswerButton = "#answerButton" + answerOrder[0];
+    incorrectAnswer1Div = "#answerDiv" + answerOrder[1];
+    incorrectAnswer1Button = "#answerButton" + answerOrder[1];
+    incorrectAnswer2Div = "#answerDiv" + answerOrder[2];
+    incorrectAnswer2Button = "#answerButton" + answerOrder[2];
+}
+
+$(".modal-buttons").click(function() {
+    console.log("*****************************");
+    console.log("answerOrder:", answerOrder);
+    console.log("correct ans",correctAnswer);
+    console.log("*********");
+    console.log("correctAnswerDiv= ",correctAnswerDiv);
+    console.log("incorrectAnswer1Div=",incorrectAnswer1Div);
+    console.log("incorrectAnswer2Div",incorrectAnswer2Div);
+})
+
+function checkAnswer() { // When an answer is selected..
+    $(correctAnswerButton).click(function () {
+        displayCorrectAnswerModal();
+        correctScore = correctScore + 1; // Update score
+        $("#correct-score-display").text(correctScore); // Push score to HTML
+        changeQuestion(); // Change question *** CIRCULAR REFERENCE TO CHANGE QUESTION OR ELSE THIS DOESNT RUN
+    });
+    $(incorrectAnswer1Button).click(function () {
+        displayIncorrectAnswerModal();
+        incorrectScore = incorrectScore + 1;
+        $("#incorrect-score-display").text(incorrectScore);
+        changeQuestion();
+    });
+    $(incorrectAnswer2Button).click(function () {
+        displayIncorrectAnswerModal();
+        incorrectScore = incorrectScore + 1;
+        $("#incorrect-score-display").text(incorrectScore);
+        changeQuestion();
     });
 }
 
@@ -195,106 +258,91 @@ function newAJAXRequest() {
     incorrectMovieTwoInfo();
 }
 
-function randomizeAnswerDivs() {
-    var answerOrder = [] // Create an array to store positions of answers
-    var pickRandom = createRandom(0,2); 
-    answerOrder.push(pickRandom); 
-    for (var i=0; i<=2; i++) {
-        if (answerOrder.indexOf(i) == -1)
-        answerOrder.push(i);
-    }
-    correctAnswerDiv = "#answerDiv" + answerOrder[0];
-    correctAnswerButton = "#answerButton" + answerOrder[0];
-    incorrectAnswer1Div = "#answerDiv" + answerOrder[1];
-    incorrectAnswer1Button = "#answerButton" + answerOrder[1];
-    incorrectAnswer2Div = "#answerDiv" + answerOrder[2];
-    incorrectAnswer2Button = "#answerButton" + answerOrder[2];
-console.log("answerOrder: "+answerOrder)
-}
-
-$(".answerButtons").click(function() { // When an answer button is clicked, change the question
-    changeQuestion();
-    })
-
-function checkAnswer() { // When an answer is selected
-    // Update the score
-    $(correctAnswerButton).click(function(){
-        correctScore = correctScore + 1;
-        $("#correct-score-display").text(correctScore);
-        changeQuestion();
-    });
-    $(incorrectAnswer1Button).click(function(){
-        incorrectScore = incorrectScore + 1;
-        $("#incorrect-score-display").text(incorrectScore);
-        changeQuestion();
-    })
-    $(incorrectAnswer2Button).click(function(){
-        incorrectScore = incorrectScore + 1;
-        $("#incorrect-score-display").text(incorrectScore);
-        changeQuestion();
-    })
-}
-
 function changeQuestion() {
     pickRandomMovies();
-    newAJAXRequest();
     pickRandomQuestion();
+    newAJAXRequest();
+    // answerOrder=[];
+    // checkAnswer();
     randomizeAnswerDivs();
-    }
+}
 
-
+// ********** COUNTER FUNCTIONS **********
 var counter = 0;
-var timeLimit = 5;
+var timeLimit = 10;
 
 function convertSeconds(s) {
-    var min = Math.floor(s/60);
-    var sec = s%60;
-        if (sec<10) {
-            return min + ":0" + sec;
-        }    
+    var min = Math.floor(s / 60);
+    var sec = s % 60;
+    if (sec < 10) {
+        return min + ":0" + sec;
+    }
     return min + ":" + sec;
 }
 
-function startTimer () {
+var timerPaused = false;
+
+var timeLeft = timeLimit;
+function startTimer() {
     $("#timer-display").text(convertSeconds(timeLimit));
-    function counting () {
-        counter++;
-        var timeLeft = timeLimit - counter;
-        $("#timer-display").text(convertSeconds(timeLeft));
-        if (timeLeft === 0) {
-            timesUp();
+        function counting() {
+            counter++;
+            timeLeft = timeLimit - counter;
+            $("#timer-display").text(convertSeconds(timeLeft));
+            if (timeLeft === 0) {
+                showTimesUp();
+            }
         }
-    }
     setInterval(counting, 1000);
 }
 
-// Actions to take if question's time limit expires
-function timesUp() {
-    $(".modal-title").text("You ran out of time!");  // **** DOESNT WORK FIX IT https://getbootstrap.com/docs/4.0/components/modal/ *****
-    $("#modal-body").text("The correct answer was... ");
-    $("#modal-body").text(correctAnswer);
-    showModal();
-    incorrectScore ++;
+function resetTimer() {
+    counter = 0;
+    timeLeft = timeLimit;
+    $("#timer-display").text(convertSeconds(timeLeft));
 }
 
-function showModal() {
-    // Write whether answer was correct or not
-    // If answer was incorrect, display correct answer
-    $("#modal1").modal("show"); // Displays the modal
+// ********** MODAL FUNCTIONS **********
+// Modals appear in 3 cases:
+//      - At the beginning of a new game
+//      - The answer is selected
+//      - The question's time limit runs out
+
+// Displays at beginning of new game and calls initializeNewGame function
+function newGameModal() {
+    $("#new-game-modal-title").text("Welcome to Movie Trivia!");
+    $("#new-game-display-text").text("You'll have one minute to answer as many questions as you can. The time begins once you click continue.")
+    $("#new-game-modal").modal("show");
 }
 
+// Modal to display if time runs out
+function showTimesUp() {
+    $("#timesup-modal-title").text("You ran out of time!");
+    $("#timesup-display-text").text("The correct answer was...");
+    $("#timesup-display-answer").text(correctAnswer);
+    $("#timesup-modal").modal("show"); // Displays the modal
+    incorrectScore = incorrectScore + 1;
+    $("#incorrect-score-display").text(incorrectScore);
+}
+
+//Modal to display if correct answer selected
+function displayCorrectAnswerModal() {
+    $("#correct-answer-modal-title").text("Nice!");
+    $("#correct-answer-display-text").text("That was the correct answer.");
+    $("#correct-answer-modal").modal("show");
+}
+
+//Modal to display if incorrect answer selected
+function displayIncorrectAnswerModal() {
+    $("#incorrect-answer-modal-title").text("Sorry!");
+    $("#incorrect-answer-display-text").text("That was the wrong answer.");
+    $("#incorrect-answer-modal").modal("show");
+}
 // --------------------------------------------------------------
 
 // CALL FUNCTIONS
-pickRandomMovies();
-pickRandomQuestion();
-newAJAXRequest();
-randomizeAnswerDivs();
-checkAnswer();
-startTimer();
+initializeNewGame();
+newGameModal();
+
 // --------------------------------------------------------------
 
-console.log("correctMovie: " + correctMovie);
-console.log("incorrectMovieOne: " + incorrectMovieOne);
-console.log("incorrectMovieTwo: " + incorrectMovieTwo);
-console.log("questionCategory: " + questionCategory);
